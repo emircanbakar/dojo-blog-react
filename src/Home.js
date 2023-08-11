cf51669693bec1c5d59ac9a441b153747bcffc22
emircanbakar <emirbakar952@gmail.com>
2023-08-12 02:54:23 +0300

    starting the react-router

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,9 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList.js";
+import useFetch from "./useFetch.js";
 
 const Home = () => {
-
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
-
-    //npx json-server --watch data/db.json --port 8000
-    useEffect(() => {
-        setTimeout(() => {
-            fetch("http://localhost:8000/blogs")
-                .then(response => {
-                    if(!response.ok){
-                        throw Error("couldn't fetch the data")
-                    }
-                    return response.json()
-                })
-                .then(data => {
-                    setBlogs(data)
-                    setIsPending(false)
-                    setError(null)
-                })
-                .catch((err) =>{
-                    setIsPending(false)
-                    setError(err.message)
-                })
-        }, 750)
-    }, [])
-
+    const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs")
+    
     return (
         <div className="home">
             { error && <div> {error} </div>}
